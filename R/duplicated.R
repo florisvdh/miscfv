2d6310ce9c36c02a5502cc5369a6c4f1d38865d0
florisvdh <floris.vanderhaeghe@inbo.be>
2023-09-28 11:55:06 +0200

    Add user_packages()

--- R/duplicated.R
+++ R/duplicated.R
@@ -22,16 +22,41 @@ duplicated_packages <- function(wide = TRUE) {
   }
 }
 
+#' List user packages
+#'
+#' @export
+user_packages <- function(libpath = .libPaths()[1], unique = TRUE, format_as_deb = FALSE) {
+  message("Selecting the packages from ", libpath, ifelse(unique, " that are not present elsewhere", ""))
+  pkgs <- installed.packages() %>%
+    as_tibble %>%
+    filter(LibPath == libpath) %>%
+    select(
+      package = Package,
+      version = Version
+    ) %>%
+    {if (unique) dplyr::anti_join(., duplicated_packages(), by = "package") else .}
+
+  if (format_as_deb) {
+    paste0("r-cran-", pkgs$package) |> tolower() |> cat()
+  } else pkgs
+}
+
 
 #' Remove duplicated packages from first element of .libPaths()
 #'
 #'
 #' @export
-remove_duplicated_pkgs <- function() {
+remove_duplicated_pkgs <- function(keep = c("sf", "terra", "ragg", "svglite")) {
   to_remove <-
     duplicated_packages(wide = FALSE) %>%
-    filter(libpath == .libPaths()[1]) %>%
+    filter(libpath == .libPaths()[1],
+           !(package %in% keep)) %>%
     pull(package)
+  if (length(to_remove) == 0L) {
+    message("No packages to remove (taking into account the 'keep' argument).\n",
+            "Use `duplicated_packages()` for more information.")
+    return(invisible(NULL))
+  }
   if (interactive()) {
     message(
       "Will remove following duplicated packages from `",
